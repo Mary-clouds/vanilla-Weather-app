853901e7b5a83a37e5c5d640e9a24ef3526e7c9f
Joseph Ribbe <social@coderwelsch.com>
2022-08-10 20:51:50 +0200

    Fixed bugs 🐞

--- src/app.js
+++ src/app.js
@@ -1,115 +1,121 @@
-function searchCity(city) {
-  let key = "1989ce48f0ddeb9155d07cad2fe7cac2";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${enterCity.value}&appid=${key}&units=metric`;
+let celciusTemp = null
+let fahrenheitTemperature = null
 
-  axios.get(apiUrl).then(showTemperature);
+let degreeElement = document.querySelector("#dayGrad")
+
+function searchForCity (city) {
+	let key = "1989ce48f0ddeb9155d07cad2fe7cac2"
+	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${ city }&appid=${ key }&units=metric`
+
+	window.axios.get(apiUrl).then(({ data }) => {
+		displayDate()
+		showTemperature(data)
+	}).catch((e) => {
+		console.error(e)
+		alert(`Couldn’t get weather data for city ${ city }`)
+	})
 }
 
-function convertFahrenheitTemperature(event) {
-  event.preventDefault();
-  //remove the link effect on fahrenheit
-  fahrenheitLink.classList.add("active");
-  celciusLink.classList.remove("active");
-  //convert fahrenheit by selecting the fahrenheit id and changing the day temperature
-  let fahrenheitTemperature = (celciusTemp + 9) / 5 + 32;
-  let degreeElement = document.querySelector("#dayGrad");
-  degreeElement.innerHTML = Math.round(fahrenheitTemperature);
+function convertFahrenheitTemperature (event) {
+	event.preventDefault()
+
+	//remove the link effect on fahrenheit
+	fahrenheitLink.classList.add("active")
+	celciusLink.classList.remove("active")
+
+	//convert fahrenheit by selecting the fahrenheit id and changing the day temperature
+	let fahrenheitTemperature = ( celciusTemp + 9 ) / 5 + 32
+	degreeElement.innerHTML = Math.round(fahrenheitTemperature)
 }
 
-function convertCelciusTemperature(event) {
-  event.preventDefault();
-  //remove the link effect on celcius
-  celciusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  //convert celcius by selecting the celcius id and changing the day temperature
-  let celciusConvertion = (fahrenheitTemperature - 32) / 1.8;
-  let celciusTemperature = document.querySelector("#dayGrad");
-  celciusTemperature.innerHTML = Math.round(celciusConvertion);
-}
-let celciusTemp = null;
-let fahrenheitLink = document.querySelector("#fahrenheitDay");
-fahrenheitLink.addEventListener("click", convertFahrenheitTemperature);
+function convertCelciusTemperature (event) {
+	event.preventDefault()
 
-let celciusLink = document.querySelector("#celciusDay");
-celciusLink.addEventListener("click", convertCelciusTemperature);
+	//remove the link effect on celcius
+	celciusLink.classList.add("active")
+	fahrenheitLink.classList.remove("active")
 
-let today = new Date();
-function displayDate() {
-  //this function will return the current date and month
-  //voir ci cette partie block le code
-
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Friday", "Saturday"];
-  let day = days[today.getDay()];
-  let date = today.getDate();
-
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "Juni",
-    "Juli",
-    "August",
-    "September",
-    "October",
-    "November",
-    "Dezember",
-  ];
-  let month = months[date.getMonth()];
-  let newDate = document.querySelector("#currentdate");
-
-  newDate.innerHTML = `${day} ${date} ${month} `;
+	//convert celcius by selecting the celcius id and changing the day temperature
+	let celciusConversion = ( fahrenheitTemperature - 32 ) / 1.8
+	degreeElement.innerHTML = Math.round(celciusConversion)
 }
 
-function showTemperature(response) {
-  let degrees = Math.round(response.data.main.temp);
-  let currentTemperature = document.querySelector("#dayGrad");
-  currentTemperature.innerHTML = `${degrees}`;
+let fahrenheitLink = document.querySelector("#fahrenheitDay")
+fahrenheitLink.addEventListener("click", convertFahrenheitTemperature)
 
-  let windElement = document.querySelector("#wind");
-  windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
-  let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
-  let precipitationElement = document.querySelector("#precipitation");
-  precipitationElement.innerHTML = `Precipitation: ${precipitation.value}%`;
-  let dateElement = document.querySelector("#currentDate");
-  dateElement.innerHTML = displayDate(response.data.dt * 1000);
+let celciusLink = document.querySelector("#celciusDay")
+celciusLink.addEventListener("click", convertCelciusTemperature)
 
-  let iconElement = document.querySelector("#icon");
-  let descriptionElement = document.querySelector("description");
+function displayDate (dateString) {
+	let newDate = document.querySelector("#currentDate")
 
-  //change the value of icon attribute to...
-  iconElement.setAttribute(
-    "src",
-    ` https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
-  );
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  //change the value of description attribute to...
-  iconElement.setAttribute("alt", `${response.data.weather[0].description}`);
+	if (dateString) {
+		date = new Date(dateString)
+	} else {
+		date = new Date()
+	}
+
+	newDate.innerHTML = date.toLocaleDateString('en-EN', {
+		weekday: "short",
+		month: "short",
+		hour: "2-digit",
+		minute: "2-digit"
+	})
 }
 
-function displayCity(event) {
-  event.preventDefault();
-  let hours = today.getHours();
-  let minutes = today.getMinutes();
-  if (hours < 10 && minutes < 10) {
-    hours = `0${hours} `;
-    minutes = `0${minutes}`;
-  }
+function showTemperature (weatherData) {
+	console.log(weatherData)
 
-  let cityElement = document.querySelector("#enterCity");
-  let newCity = document.querySelector("#cityInput");
+	let degrees = Math.round(weatherData.main.temp)
+	degreeElement.innerHTML = `${ degrees }`
 
-  if (cityElement.value.length >= 3) {
-    newCity.innerHTML = `${cityElement.value}  ${hours}:${minutes}`;
-    searchCity(cityElement.value);
-  } else {
-    alert(` please enter a city`);
-  }
-  displayDate();
-  displayForecast();
+	let windElement = document.querySelector("#wind")
+	windElement.innerHTML = `Wind: ${ Math.round(weatherData.wind.speed) } km/h`
+
+	let humidityElement = document.querySelector("#humidity")
+	humidityElement.innerHTML = `Humidity: ${ weatherData.main.humidity }%`
+
+	let precipitationElement = document.querySelector("#precipitation")
+	precipitationElement.innerHTML = `Precipitation: ${ precipitation.value }%`
+
+	displayDate(weatherData.dt * 1000)
+
+	let iconElement = document.querySelector("#icon")
+
+	//change the value of icon attribute to...
+	iconElement.setAttribute(
+		"src",
+		` https://openweathermap.org/img/wn/${ weatherData.weather[0].icon }.png`,
+	)
+
+	let descriptionElement = document.querySelector("#description")
+	descriptionElement.innerHTML = weatherData.weather[0].description
 }
 
-let citynames = document.querySelector("#citiesTemperature");
-citynames.addEventListener("submit", displayCity);
+function submitWeatherForm (event) {
+	event.preventDefault()
+
+	const today = new Date()
+
+	let hours = today.getHours()
+	let minutes = today.getMinutes()
+	if (hours < 10 && minutes < 10) {
+		hours = `0${ hours } `
+		minutes = `0${ minutes }`
+	}
+
+	let cityInput = document.querySelector("#enterCity")
+	let newCity = document.querySelector("#cityInput")
+
+	if (cityInput.value.length >= 3) {
+		newCity.innerHTML = `${ cityInput.value }  ${ hours }:${ minutes }`
+		searchForCity(cityInput.value)
+	} else {
+		alert(`Please enter a city …`)
+	}
+
+	return false;
+}
+
+let weatherForm = document.querySelector("#weatherForm")
+weatherForm.addEventListener("submit", submitWeatherForm)
